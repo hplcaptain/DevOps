0e7b384fc7212d35d99ffa9affb2f730e81f2c71
hplcaptain <hploffice365@gmail.com>
2026-06-29 14:49:37 +0530

    Complete Assignment 6

--- myfirst-app/backend/app.py
+++ myfirst-app/backend/app.py
@@ -2,17 +2,22 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/process', methods=['POST'])
-def process():
+# Health check
+@app.route("/", methods=["GET"])
+def home():
+    return "Backend is running successfully on AWS ECS!"
 
+# Process API
+@app.route("/process", methods=["POST"])
+def process():
     data = request.get_json()
 
-    name = data.get('name')
-    email = data.get('email')
+    name = data.get("name")
+    email = data.get("email")
 
     return jsonify({
         "message": f"User {name} with email {email} registered successfully"
     })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- myfirst-app/frontend/server.js
+++ myfirst-app/frontend/server.js
@@ -6,6 +6,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const axios = require("axios");
+
+const BACKEND_URL = "http://100.24.58.12:5000/";
+
 app.set("view engine", "ejs");
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,12 +19,22 @@ app.get("/", (req, res) => {
     res.render("index", { result: null });
 });
 
-app.post("/submit", (req, res) => {
+app.post("/submit", async (req, res) => {
     const { name, email } = req.body;
-    console.log(`Received submission: Name - ${name}, Email - ${email}`);
-    res.send("Form submitted successfully!");
+
+    try {
+        const response = await axios.post(`${BACKEND_URL}/process`, {
+            name,
+            email
+        });
+
+        res.send(response.data);
+
+    } catch (error) {
+        res.status(500).send("Backend not reachable");
+    }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server is running on port ${PORT}`);
 });
